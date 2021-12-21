2e5e1b5eedf91d8154330e431ac32b4498bdc95c
Klas Mellbourn <klas@mellbourn.net>
2021-12-21 22:27:49 +0100

    added my copyright

--- background.js
+++ background.js
@@ -1,5 +1,6 @@
 // Copyright (c) 2012 The Chrome Authors. All rights reserved.
 // Copyright (c) 2016 Andreas Källberg. All rights reserved.
+// Copyright (c) 2021 Klas Mellbourn. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
