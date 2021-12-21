e0e84eaf865f95f3386fe91427c30a9365c90dca
Klas Mellbourn <klas@mellbourn.net>
2021-12-21 23:10:43 +0100

    migrated to manifest v3

--- background.js
+++ background.js
@@ -5,6 +5,50 @@
 // found in the LICENSE file.
 
 
+const createNewWindow = (
+  tab,
+  windowType,
+  windowBounds,
+  isFullscreen,
+  isFocused
+) => {
+  // new window options
+  const opts = {
+    tabId: tab.id,
+    type: windowType,
+    focused: isFocused,
+    incognito: tab.incognito,
+    ...windowBounds,
+  };
+
+  if (isFullscreen) {
+    return new Promise((resolve) => {
+      chrome.windows.create(opts, (newWin) => {
+        if (newWin !== undefined) {
+          // this timeout is gross but necessary.
+          // updating immediately fails
+          setTimeout(() => {
+            chrome.windows.update(newWin.id, { state: "fullscreen" }, () => {
+              resolve([newWin, tab]);
+            });
+          }, 1000);
+        }
+      });
+    });
+  }
+
+  return new Promise((resolve, reject) => {
+    chrome.windows.create(opts, (newWin) => {
+      if (newWin !== undefined) {
+        resolve([newWin, tab]);
+      } else {
+        reject("Could not create new window");
+      }
+    });
+  });
+};
+
+
 // Takes a function which takes a callback as its last argument
 // and converts it to a promise, by using "resolve" as the callback.
 function toPromise(fun, ...args){
